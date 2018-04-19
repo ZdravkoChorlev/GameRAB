c725f651b81583a6637920e9af26d3d267c6b42b
ZdravkoGeorgiev <30442544+ZdravkoChorlev@users.noreply.github.com>
2018-04-20 00:11:32 +0300

    first
    My First unity game

---
+++ Assets/Scripts/PlayGame.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.SceneManagement;
+using UnityEngine.UI;
+
+
+public class PlayGame : MonoBehaviour {
+    public Button playB;
+    public Button ExitB;
+
+    void Start()
+    {
+        ExitB.onClick.AddListener(() => { Application.Quit();});
+    }
+	void Update () {
+
+	}
+}
---
+++ Assets/Scripts/PlayerController.cs
@@ -0,0 +1,64 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class PlayerController : MonoBehaviour
+{
+    public GameObject portal;
+    public GameObject ramp;
+    public GameObject player;
+    public float speed;
+    public Text countText;
+    public Text winText;
+    private Rigidbody rb;
+    private int count;
+    
+    void Start()
+    {
+        count = 0;
+        rb = GetComponent<Rigidbody>();
+        SetCountText();
+        winText.text = " ";
+    }
+
+    void FixedUpdate()
+    {
+        float moveHorizontal = Input.GetAxis("Horizontal");
+        float moveVertical = Input.GetAxis("Vertical");
+
+        Vector3 movement = new Vector3(moveHorizontal, 0.0f, moveVertical);
+        rb.AddForce(movement * speed);
+    }
+    void OnTriggerEnter(Collider other)
+    {
+        if (other.gameObject.CompareTag("Teleport"))
+        {
+            SceneManager.LoadScene(1);
+        }
+
+        if (other.gameObject.CompareTag("Pick Up"))
+        {
+
+            other.gameObject.SetActive(false);
+            count = count + 1;
+            SetCountText();
+        }
+
+        if (other.gameObject.CompareTag("Ramp"))
+        {
+           ramp.SetActive(true);
+        }
+    }
+
+    void SetCountText()
+    {
+        if (count >= 2)
+        {
+            winText.text = "You win";
+            portal.SetActive(true);
+        }
+        countText.text = "Count" + count.ToString();
+    }
+}
